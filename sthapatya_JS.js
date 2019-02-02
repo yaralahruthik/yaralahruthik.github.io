86aa0856850335b3dc27625df8e8bdc5b5deee82
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2019-02-02 19:08:09 +0530

    Major Changes
    Dangerous Commit

--- sthapatya_JS.js
+++ sthapatya_JS.js
@@ -1,15 +1,15 @@
 // JavaScript Document
 
-function menuAnimate(){
+function menuAnimate() {
     var x = document.getElementById("navbar");
-    if(x.className === "menu"){
-        x.className += " responsive";
+    if (x.className === "menu") {
+        x.className = "menu responsive";
     }
     else {
         x.className = "menu";
     }
     var y = document.getElementById("changeMenu");
-    if(y.className === "fa fa-bars"){
+    if (y.className === "fa fa-bars"){
         y.className = "fa fa-times";
     }
     else {
@@ -17,6 +17,26 @@ function menuAnimate(){
     }
 }
 
+var countDownDate = new Date("March 15, 2019 00:00:00").getTime();
+var z = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s till";
+
+if (distance < 0) {
+clearInterval(z);
+document.getElementById("countdown").innerHTML = "Almost, ready to reveal";
+}
+}, 1000);
+
+
+/*
 window.onscroll = function() {scrollMenu()};
 
 function scrollMenu() {
@@ -26,3 +46,4 @@ function scrollMenu() {
     document.getElementById("container").style.top = "-100px";
     }
 }
+*/
