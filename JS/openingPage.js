d9c34dec5a1a7c0d2f849fb6cf82d54b1f12a333
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2020-02-29 18:47:26 +0530

    Brief Page

--- JS/openingPage.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-document.body.classList.add('js-loading');
-
-window.addEventListener("load", showPage);
-
-function showPage() {
-  document.body.classList.remove('js-loading');
-}
